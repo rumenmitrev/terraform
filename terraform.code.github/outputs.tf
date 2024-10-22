327860ec16e0826f125c18ed360e0408216437fe
rumenmitrev <rumen.mitrev@abv.bg>
2024-10-22 15:15:05 +0300

    try function

--- terraform.code.github/outputs.tf
+++ terraform.code.github/outputs.tf
@@ -2,7 +2,7 @@ output "repo_names" {
   value = { for i in github_repository.ibasi : i.name => {
     ssh-url   = i.http_clone_url,
     clone_url = i.ssh_clone_url,
- #   home_page = i.pages[0].html_url
+    home_page = try(i.pages[0].html_url, "no page")
     }
   }
   description = "repo names and url"
