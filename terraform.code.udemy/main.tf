737510f819deef8390f713c44b70e4b0822c7fc6
rumenmitrev <rumen.mitrev@abv.bg>
2024-11-14 22:20:25 +0200

    udemy/init

---
+++ terraform.code.udemy/main.tf
@@ -0,0 +1,13 @@
+data "aws_ami" "server_ami" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami = data.aws_ami.server_ami.id
+  instance_type = "t2.micro"
+}
---
+++ terraform.code.udemy/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.70.0"
+    }
+  }
+}
+
+provider "aws" {
+  shared_config_files      = ["$HOME/.aws/config"]
+  shared_credentials_files = ["$HOME/.aws/credentials"]
+  region                   = var.aws_region
+}
