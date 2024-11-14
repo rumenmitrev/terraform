9ee34c09d12a7eedaee4b6d9924047b4bdcde1a8
rumenmitrev <rumen.mitrev@abv.bg>
2024-11-15 00:15:23 +0200

    udemy/ install software via provisioners and scripts / connection block / key_pair resource

--- terraform.code.udemy/main.tf
+++ terraform.code.udemy/main.tf
@@ -7,7 +7,41 @@ data "aws_ami" "server_ami" {
   }
 }
 
+resource "aws_key_pair" "ec2-key-pair" {
+  key_name   = "rm-key"
+  public_key = file(var.public_key_path)
+}
+
 resource "aws_instance" "example" {
-  ami = data.aws_ami.server_ami.id
+  count = 1
+  #   ami           = data.aws_ami.server_ami.id  # use datasource
+  ami           = lookup(var.amis, var.aws_region) # use lookup function to search in map structure
+  key_name      = aws_key_pair.ec2-key-pair.key_name
   instance_type = "t2.micro"
+  tags = {
+    Name = "my-first-ec2-${count.index}"
+  }
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      max_price = 0.0031
+    }
+  }
+
+  provisioner "file" {
+    source      = "./install_nginx.sh"
+    destination = "/tmp/install_nginx.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/install_nginx.sh",
+      "sudo /tmp/install_nginx.sh"
+    ]
+  }
+  connection {
+    host        = self.public_dns
+    type        = "ssh"
+    user        = var.ec2_user
+    private_key = file(var.private_key_path)
+  }
 }
--- terraform.code.udemy/providers.tf
+++ terraform.code.udemy/providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.70.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.76.0"
     }
   }
 }
